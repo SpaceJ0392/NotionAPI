927fc74020f7b2724581a1419fd5d52eb8c8dbc3
cosmosJ <jwj0392@gmail.com>
2025-04-23 14:05:34 +0900

    change scheduler.js drop node-cron

--- habbitTracker/scheduler.js
+++ habbitTracker/scheduler.js
@@ -1,11 +1,8 @@
-const cron = require('node-cron');
 const duplicateAllPages = require('./duplicate');
 
 // 매일 자정 (00:00)에 실행
-cron.schedule('0 0 * * *', () => {
+(async () => {
   console.log(`[${new Date().toLocaleString()}] 🕛 자동 복제 시작`);
   duplicateAllPages().catch(console.error);
-});
+})();
 
-// 프로세스를 계속 살려두기 위한 로그
-console.log('🟢 스케줄러 실행 중... (Ctrl+C로 종료)');
