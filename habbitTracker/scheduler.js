488b6f8259cd44074a3ef5712eea0aadf06bea38
cosmosJ <jwj0392@gmail.com>
2025-04-24 19:59:07 +0900

    Update scheduler.js : UTC to KST

--- habbitTracker/scheduler.js
+++ habbitTracker/scheduler.js
@@ -2,7 +2,11 @@ const duplicateAllPages = require('./duplicate');
 
 // 매일 자정 (00:00)에 실행
 (async () => {
-  console.log(`[${new Date().toLocaleString()}] 🕛 자동 복제 시작`);
+  const logTime = require('dayjs')()
+  .tz('Asia/Seoul')
+  .format('YYYY-MM-DD HH:mm:ss');
+
+  console.log(`[${logTime}] 🕛 자동 복제 시작`);
   duplicateAllPages().catch(console.error);
 })();
 
