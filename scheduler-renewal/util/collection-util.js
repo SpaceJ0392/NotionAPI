af3b597d5cc359731c5dd260ef8149e379208b07
cosmosJ <jwj0392@gmail.com>
2026-01-15 10:24:10 +0900

    (fix) fixed timezone problem

--- scheduler-renewal/util/collection-util.js
+++ scheduler-renewal/util/collection-util.js
@@ -1,14 +1,25 @@
 import NotionUtil from "./notion-util.js";
+import * as constraint from "../notion-const.js";
 
-export default class CollectionUtil{
+export default class CollectionUtil {
 
-    static getNotionDistinct(list, target){
-        if(!Array.isArray(list)) return [];
+    static getNotionDistinct(list, target, orderBy = constraint.TABLE_COLUMNS.DATE) {
+        if (!Array.isArray(list)) return [];
         const res = new Map();
 
-        list.forEach(item =>{
+        list.forEach(item => {
             const key = NotionUtil.getNotionPagePropertyName(item, target);
-            if (key && !res.has(key)) { res.set(key, item); }
+            const currentDate = new Date( NotionUtil.getNotionPagePropertyName(item, orderBy));
+
+            if (!res.has(key)) { res.set(key, item); }
+            else {
+                const existingItem = res.get(key);
+                const existingDate = new Date(NotionUtil.getNotionPagePropertyName(existingItem, orderBy));
+
+                if (currentDate > existingDate) {
+                    res.set(key, item);
+                }
+            }
         });
 
         return Array.from(res.values());
--- scheduler-renewal/util/date-util.js
+++ scheduler-renewal/util/date-util.js
@@ -1,17 +1,27 @@
-export default class DateUtil{
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
 
-    static beforeNDays(targetDays){
-        const date = new Date();
-        date.setDate(date.getDate() - targetDays)
-        return date.toISOString().split('T')[0];
+// 플러그인 설정 (클래스 밖에서 한 번만 실행하면 됩니다)
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+
+export default class DateUtil {
+
+    static #TIME_ZONE = "Asia/Seoul";  // 기본 타임존을 서울로 설정
+
+    static beforeNDays(targetDays) {
+        return dayjs().tz(this.#TIME_ZONE).subtract(targetDays, "day").format('YYYY-MM-DD');
     }
 
-    static getTodayDate(){
-        return new Date().toISOString().split('T')[0];
+    static getTodayDate() {
+        return dayjs().tz(this.#TIME_ZONE).format('YYYY-MM-DD');
     }
 
     static getTodayWeekday() {
-    const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
-    return weekdays[new Date().getDay()]; // 오늘 요일 반환
-}
+        const weekdays = ["일", "월", "화", "수", "목", "금", "토"];
+        const dayIndex = dayjs().tz(this.#TIME_ZONE).day();
+        return weekdays[dayIndex]; // 오늘 요일 반환
+    }
 }
